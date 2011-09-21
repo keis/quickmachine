caefd065b2030f59cc9c7caee7d987ef69ea2ea8
David Keijser <keijser@gmail.com>
2011-09-21 23:12:48 +0200

    refactor call history out of command list

--- src/quickmachine/CommandList.java
+++ src/quickmachine/CommandList.java
@@ -10,33 +10,10 @@ public class CommandList extends ArrayList<Call> {
         this.statem = statem;
     }
     
-    String formatHistory(List<Call> history, Call lastCall, DynamicResult lastResult, DynamicState lastState) {
-        StringBuilder sb = new StringBuilder();
-        int lastnl = 0;
-        for(Call call : history) {
-            sb.append(call);
-            sb.append(", ");
-            if(sb.length() - lastnl > 70) {
-                sb.append("\n");
-                lastnl = sb.length();
-            }
-        }
-        
-        sb.append('\n');
-        sb.append("Post conditions of ");
-        sb.append(lastCall);
-        sb.append(" does not hold:");
-        sb.append("\nresult: ");
-        sb.append(lastResult);
-        sb.append("\nstate: ");
-        sb.append(lastState);
-        return sb.toString();
-    }
-    
     public State execute() {
         DynamicState state = this.statem.initialState();
         DynamicResult res = null;
-        List<Call> history = new ArrayList<>();
+        History history = new History();
         
         for(Call call : this) {
             //System.out.print(call.toString() + " @ " + state.toString());
@@ -50,11 +27,11 @@ public class CommandList extends ArrayList<Call> {
             
             //System.out.println(res.toString());
                         
-            history.add(call);
+            history.add(call, state, res);
             
             // Check post conditions (could Assert be used to closer integrate with JUnit?)
             if (!call.postcondition(state, res)) {
-                System.out.print(this.formatHistory(history, call, res, state));
+                System.out.print(history.format());
                 
                 throw new RuntimeException("Post conditions of " + call.toString() + " does not hold");
             }
---
+++ src/quickmachine/History.java
@@ -0,0 +1,62 @@
+package quickmachine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class History {
+
+    private class Item {
+
+        public Call call;
+        public State state;
+        public Result result;
+
+        Item(Call call, State state, Result result) {
+            this.call = call;
+            this.state = state;
+            this.result = result;
+        }
+    }
+    List<Item> history;
+
+    public History() {
+        this.history = new ArrayList<>();
+    }
+
+    public boolean add(Call call, State state, Result result) {
+        return this.history.add(new Item(call, state, result));
+    }
+
+    private Item last() throws IndexOutOfBoundsException {
+        return this.history.get(this.history.size() - 1);
+    }
+
+    public String format() {
+        StringBuilder sb = new StringBuilder();
+        int lastnl = 0;
+        for (Item item : this.history) {
+            sb.append(item.call);
+            sb.append(", ");
+            if (sb.length() - lastnl > 70) {
+                sb.append("\n");
+                lastnl = sb.length();
+            }
+        }
+        Item last = this.last();
+
+        sb.append('\n');
+        sb.append("Post conditions of ").append(last.call).append(" does not hold\n");
+        sb.append("result: ").append(last.result).append("\n");
+        sb.append("state: ").append(last.state).append("\n");
+
+        return sb.toString();
+    }
+
+    public String toString() {
+        Item last = this.last();
+        return "History{"
+                + this.history.size() + "call(s), "
+                + last.call.toString() + ", "
+                + last.result.toString() + "}";
+    }
+}
